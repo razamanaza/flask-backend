b60f1fa144241019e3e0d888a1c785460d04a94b
vagrant <vagrant@scotchbox>
2020-03-04 05:56:31 +0000

    Rewrote static pages on Jinja

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
-@app.errorhandler(404) 
-def not_found(e): 
-  return render_template('404.html') 
+@app.route('/visual')
+def visual():
+  return render_template('visual.html')
+
+@app.route('/inspirations')
+def inspirations():
+  return render_template('inspirations.html')
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template('404.html')
 
 if __name__ =='__main__':
-    app.run(debug=True,port=8080,host='0.0.0.0')
+  app.run(debug=True,port=8080,host='0.0.0.0')
