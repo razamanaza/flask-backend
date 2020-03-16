af2ec09ee5d0b92e89574845d5445132baba825e
vagrant <vagrant@scotchbox>
2020-03-16 02:52:36 +0000

    Finished load data function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from mongoengine import *
 import os
+import csv
 
 app = Flask(__name__)
 
@@ -10,6 +11,9 @@ app.config.from_object('config')
 
 class Country(Document):
     name = StringField()
+    agriculture = DictField()
+    industry = DictField()
+    service = DictField()
 
 @app.route('/')
 def index():
@@ -17,23 +21,8 @@ def index():
 
 @app.route('/visual')
 def visual():
-  #russia = Country(name = 'Russia')
-  #russia.save()
-  #nz = Country(name = 'New Zealand')
-  #nz.save()
-  """ countries = []
-  for c in Country.objects:
-    countries.append(c.name)
-  return render_template('visual.html', countries = countries) """
-
-  for file in os.listdir(app.config['FILES_FOLDER']):
-    filename = os.fsdecode(file)
-    path = os.path.join(app.config['FILES_FOLDER'], filename)
-    f = open(path)
-    r = csv.reader(f)
-    d = list(r)
-    for data in d:
-        print(data)
+  country = Country.objects.get(name = 'Russia')
+  return render_template('visual.html', countries = country.to_json())
 
 @app.route('/inspirations')
 def inspirations():
@@ -59,6 +48,42 @@ def addCountry(country_id):
 @app.route('/countries/<country_id>', methods=['DELETE'])
 def deleteCountry(country_id):
   return
+
+@app.route('/loadData')
+def loadData():
+  #Clean table before loading data
+  for c in Country.objects:
+    c.delete()
+  for file in os.listdir(app.config['FILES_FOLDER']):
+    filename = os.fsdecode(file)
+    path = os.path.join(app.config['FILES_FOLDER'], filename)
+    f = open(path)
+    r = csv.DictReader(f)
+    d = list(r)
+    dataset = filename.replace(".csv","")
+    for data in d:
+        dict = {}
+        for key in data:
+          if key == 'country':
+            try:
+              Country.objects.get(name = data[key])
+            except DoesNotExist:
+              isCountryExists = False
+            else:
+              isCountryExists = True
+
+            if isCountryExists:
+              country = Country.objects.get(name = data[key])
+            else:
+              country = Country(name = data[key])
+
+          else:
+            dict[key] = data[key]
+        country[dataset] = dict
+        country.save()
+
+  return render_template('success.html'), 202
+
 
 if __name__ =='__main__':
   app.run(debug=True,port=8080,host='0.0.0.0')
